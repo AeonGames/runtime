03140dbca4079b92caac7473bb0dac2795569746
Rodrigo Hernandez <kwizatz@aeongames.com>
2015-10-05 15:31:02 -0600

    Made freetype optional as the other 2 packages.

--- packages/freetype.cmake
+++ packages/freetype.cmake
@@ -3,7 +3,17 @@
 
 include(functions)
 
-download("http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz" "freetype-2.6.tar.gz")
-decompress("freetype-2.6.tar.gz" "freetype-2.6")
+option(RUNTIME_BUILD_FREETYPE "Build the freetype true type font library.")
 
-add_subdirectory("${BUILD_DIRECTORY}/freetype-2.6" "${CMAKE_BINARY_DIR}/freetype-2.6")
+if(RUNTIME_BUILD_FREETYPE)
+	download("http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz" "freetype-2.6.tar.gz")
+	decompress("freetype-2.6.tar.gz" "freetype-2.6")
+	if(BUILD_SHARED_LIBS AND MSVC)
+		message(STATUS "Warning!!! the Freetype Shared Library for VS does not generate an export library.")
+	endif()
+	add_subdirectory("${CMAKE_SOURCE_DIR}/freetype-2.6" "${CMAKE_BINARY_DIR}/freetype-2.6")	
+	# Set these variables in case a latter package has FT2 as a dependency.
+	set(FREETYPE_INCLUDE_DIR_freetype2 "${CMAKE_SOURCE_DIR}/freetype-2.6/include" CACHE PATH "FreeType2 Include Directory")
+	set(FREETYPE_INCLUDE_DIR_ft2build  "${CMAKE_SOURCE_DIR}/freetype-2.6/include" CACHE PATH "ft2build Include Directory")
+	set(FREETYPE_LIBRARIES "freetype" CACHE INTERNAL "freetype library")
+endif()
