97b0ddb1323057a16c1059bb8571e003fc663e2e
Rodrigo Hernandez <kwizatz@aeongames.com>
2015-12-10 12:16:01 -0600

    glslang 3.0 properly compiles with VS2015.

--- recipes/glslang.cmake
+++ recipes/glslang.cmake
@@ -5,8 +5,12 @@ include(functions)
 
 option(RUNTIME_BUILD_GLSLANG "Build the glslang GLSL verifier.")
 
-if(RUNTIME_BUILD_ZLIB)
+if(RUNTIME_BUILD_GLSLANG)
 	download("https://github.com/KhronosGroup/glslang/archive/3.0.tar.gz" "glslang-3.0.tar.gz")
 	decompress("glslang-3.0.tar.gz" "glslang-3.0")
+	file(READ ${BUILD_DIRECTORY}/glslang-3.0/CMakeLists.txt GLSLANG_CMAKELISTS)
+	string(REGEX REPLACE "(\\s*)[^#](set\\(CMAKE_GENERATOR_TOOLSET)" "\\1#\\2"
+			GLSLANG_CMAKELISTS_PATCHED "${GLSLANG_CMAKELISTS}")
+	file(WRITE ${BUILD_DIRECTORY}/glslang-3.0/CMakeLists.txt "${GLSLANG_CMAKELISTS_PATCHED}")
 	add_subdirectory("${BUILD_DIRECTORY}/glslang-3.0" "${CMAKE_BINARY_DIR}/glslang-3.0")
 endif()
