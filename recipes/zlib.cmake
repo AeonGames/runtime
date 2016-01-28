daf19ada9c29f39e6a5d22de6f5a53bcdd4c9464
Rodrigo Hernandez <kwizatz@aeongames.com>
2016-01-28 11:33:52 -0600

    Might finally have fixed zlib-protobuf issue.

--- recipes/zlib.cmake
+++ recipes/zlib.cmake
@@ -14,6 +14,16 @@ if(RUNTIME_BUILD_ZLIB)
 			ZLIB_CMAKELISTS_PATCHED "${ZLIB_CMAKELISTS}")
 	file(WRITE ${BUILD_DIRECTORY}/zlib-1.2.8/CMakeLists.txt "${ZLIB_CMAKELISTS_PATCHED}") 
 	add_subdirectory("${BUILD_DIRECTORY}/zlib-1.2.8" "${CMAKE_BINARY_DIR}/zlib-1.2.8")
+	set(ZLIB_FOUND ON CACHE INTERNAL "Link against the zlib we're already building" FORCE)
 	set(ZLIB_INCLUDE_DIRS "${BUILD_DIRECTORY}/zlib-1.2.8" "${CMAKE_BINARY_DIR}/zlib-1.2.8" CACHE INTERNAL "Local zlib include directories" FORCE)
-	set(ZLIB_INCLUDE_DIR ${ZLIB_INCLUDE_DIRS}  CACHE PATH "Local zlib include directories" FORCE)
+	set(ZLIB_INCLUDE_DIR ${ZLIB_INCLUDE_DIRS}  CACHE INTERNAL "Local zlib include directories" FORCE)
+	get_property(ZLIB_LIBRARY_TEST TARGET zlib PROPERTY OUTPUT_NAME)
+	set(ZLIB_LIBRARY zlib CACHE INTERNAL "Local zlib" FORCE)
+    if(NOT TARGET ZLIB::ZLIB)
+		add_library(ZLIB::ZLIB UNKNOWN IMPORTED)
+		set_target_properties(ZLIB::ZLIB PROPERTIES
+			IMPORTED_LOCATION_RELEASE "${CMAKE_BINARY_DIR}/bin/$(Configuration)/zlib.lib"
+			IMPORTED_LOCATION_DEBUG "${CMAKE_BINARY_DIR}/bin/$(Configuration)/zlibd.lib"
+			INTERFACE_INCLUDE_DIRECTORIES "${ZLIB_INCLUDE_DIRS}")
+    endif()	
 endif()
