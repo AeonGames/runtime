e37a2b671ed5457366e7f89ef11bac209508ec9a
Rodrigo Hernandez <kwizatz@aeongames.com>
2016-01-28 17:52:39 -0600

    Sticking to the latest stable release of protobuf.

--- recipes/protobuf.cmake
+++ recipes/protobuf.cmake
@@ -6,7 +6,7 @@ include(functions)
 option(RUNTIME_BUILD_PROTOBUF "Build the google proto buffers library.")
 
 if(RUNTIME_BUILD_PROTOBUF)
-	gitclone("https://github.com/google/protobuf.git" "protobuf")
+	gitclonetag("https://github.com/google/protobuf.git" "protobuf" "v3.0.0-beta-2")
 	set(protobuf_BUILD_TESTS OFF CACHE BOOL "We have our own gtest instance." FORCE)
 	if(RUNTIME_BUILD_ZLIB)
 		set(protobuf_WITH_ZLIB ON CACHE INTERNAL "Link against the zlib we're already building" FORCE)
