9c50f22e6c1a42fe0e9765ed8e49419c387316a3
Rodrigo Hernandez <kwizatz@aeongames.com>
2016-04-27 17:52:00 -0600

    Work in Progress: Fix zlib usage on protocol buffers when cmake >= 3.0.0

--- recipes/zlib.cmake
+++ recipes/zlib.cmake
@@ -14,13 +14,13 @@ if(RUNTIME_BUILD_ZLIB)
     string(REGEX REPLACE "([ \t]*)add_test" "\\1#add_test"
             ZLIB_CMAKELISTS_PATCHED "${ZLIB_CMAKELISTS}")
     file(WRITE ${BUILD_DIRECTORY}/zlib-1.2.8/CMakeLists.txt "${ZLIB_CMAKELISTS_PATCHED}")
-    set(ZLIB_INCLUDE_DIRS "${BUILD_DIRECTORY}/zlib-1.2.8" "${CMAKE_BINARY_DIR}/zlib-1.2.8" CACHE INTERNAL "Local zlib include directories" FORCE)
-    set(ZLIB_INCLUDE_DIR ${ZLIB_INCLUDE_DIRS}  CACHE INTERNAL "Local zlib include directories" FORCE)
+    set(ZLIB_INCLUDE_DIRS "${BUILD_DIRECTORY}/zlib-1.2.8" "${CMAKE_BINARY_DIR}/zlib-1.2.8" CACHE PATH "Local zlib include directories" FORCE)
+    set(ZLIB_INCLUDE_DIR ${ZLIB_INCLUDE_DIRS} CACHE PATH "Local zlib include directories" FORCE)
     include_directories(${ZLIB_INCLUDE_DIRS})
     add_subdirectory("${BUILD_DIRECTORY}/zlib-1.2.8" "${CMAKE_BINARY_DIR}/zlib-1.2.8")
     set(ZLIB_FOUND ON CACHE INTERNAL "Link against the zlib we're already building" FORCE)
     get_property(ZLIB_LIBRARY_TEST TARGET zlib PROPERTY OUTPUT_NAME)
-    set(ZLIB_LIBRARY zlib CACHE INTERNAL "Local zlib" FORCE)
+    set(ZLIB_LIBRARY zlib CACHE STRING "Local zlib" FORCE)
     if(NOT TARGET ZLIB::ZLIB)
         add_library(ZLIB::ZLIB UNKNOWN IMPORTED)
         if(MSVC)
@@ -37,6 +37,13 @@ if(RUNTIME_BUILD_ZLIB)
                     IMPORTED_LOCATION "${ZLIB_LOCATION}"
                     IMPORTED_LOCATION_RELEASE "${ZLIB_LOCATION_RELEASE}"
                     IMPORTED_LOCATION_DEBUG "${ZLIB_LOCATION_DEBUG}"
+                    INTERFACE_INCLUDE_DIRECTORIES "${ZLIB_INCLUDE_DIRS}")
+			else()
+				message(STATUS "Location: $<TARGET_PROPERTY:ZLIB::ZLIB,LOCATION>")
+                set_target_properties(ZLIB::ZLIB PROPERTIES
+                    IMPORTED_LOCATION "$<TARGET_PROPERTY:ZLIB::ZLIB,LOCATION>"
+                    IMPORTED_LOCATION_RELEASE "$<TARGET_PROPERTY:LOCATION_RELEASE>"
+                    IMPORTED_LOCATION_DEBUG "$<TARGET_PROPERTY:LOCATION_DEBUG>"
                     INTERFACE_INCLUDE_DIRECTORIES "${ZLIB_INCLUDE_DIRS}")
             endif()
         endif()
