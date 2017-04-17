cd7002fb3bb98bedf51f6802eb65e68254c5e2f1
Rodrigo Hernandez <kwizatz@aeongames.com>
2017-04-17 12:10:52 -0600

    Bumped protobuf version to 3.2.0

--- recipes/protobuf.cmake
+++ recipes/protobuf.cmake
@@ -1,4 +1,4 @@
-# Copyright 2016 AeonGames, Rodrigo Hernandez
+# Copyright 2016-2017 AeonGames, Rodrigo Hernandez
 # Licensed under the terms of the Apache 2.0 License.
 
 include(functions)
@@ -6,7 +6,7 @@ include(functions)
 option(RUNTIME_BUILD_PROTOBUF "Build the google proto buffers library.")
 
 if(RUNTIME_BUILD_PROTOBUF)
-    gitclonetag("https://github.com/google/protobuf.git" "protobuf" "v3.1.0")
+    gitclonetag("https://github.com/google/protobuf.git" "protobuf" "v3.2.0")
     set(protobuf_BUILD_TESTS OFF CACHE BOOL "We have our own gtest instance." FORCE)
     set(protobuf_MSVC_STATIC_RUNTIME OFF CACHE BOOL "Dynamic Runtime is the default." FORCE)
     set(protobuf_BUILD_SHARED_LIBS ON CACHE BOOL "Force shared libraries." FORCE)
