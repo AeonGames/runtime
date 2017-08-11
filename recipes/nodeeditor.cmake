b923e9429e40cc8c7513d20ede394022f2b1a3bc
Rodrigo Hernandez <kwizatz@aeongames.com>
2017-08-10 22:02:14 -0600

    Trying to fix deployment of nodes editor.
    Changing how the install commands are injected.
    
    [deploy]

--- recipes/nodeeditor.cmake
+++ recipes/nodeeditor.cmake
@@ -8,8 +8,9 @@ option(RUNTIME_BUILD_NODEEDITOR "Build the Qt Node Editor Widget.")
 if(RUNTIME_BUILD_NODEEDITOR)
     gitclonecommit("https://github.com/paceholder/nodeeditor.git" "nodeeditor" "b2aa9bc166c2b06d543049d74e6c70b696f97c42")
     set(BUILD_EXAMPLES OFF CACHE INTERNAL "Node Editor Examples DISABLED." FORCE)
-    file(MD5 ${BUILD_DIRECTORY}/nodeeditor/CMakeLists.txt NODES_CMAKELISTS_MD5)
-    if(NODES_CMAKELISTS_MD5 EQUAL 3e9380df394ed5a756df15879ec86e03)
+    file(READ ${BUILD_DIRECTORY}/nodeeditor/CMakeLists.txt NODES_CMAKELISTS_TXT)
+    if(NOT ${NODES_CMAKELISTS_TXT} MATCHES "install")
+        message(STATUS "Patching node editor CMakeLists.txt")
         file(APPEND ${BUILD_DIRECTORY}/nodeeditor/CMakeLists.txt "\n"
             "install(TARGETS nodes\n"
             "RUNTIME DESTINATION bin\n"
